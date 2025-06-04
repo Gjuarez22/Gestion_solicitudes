5a4264816189f3630af8755388c2e3452a607332
juarezG2025 <gerardo.juarez@contecsalatam.com>
2025-06-04 17:02:20 -0600

    Aplicacion responsiva, añadido datatable, y flujos para los SP

---
+++ Models/SpListadoSoliciturdes.cs
@@ -0,0 +1,14 @@
+﻿namespace GestionSolicitud.Models
+{
+    public class SpListadoSoliciturdes
+    {
+        public int IdSolicitud { get; set; }
+        public DateTime Creado { get; set; }
+        public string DocNum { get; set; }
+        public string Comentarios { get; set; }
+        public string Tipo_Solicitud { get; set; }
+        public string Nombre { get; set; }
+        public string NombreArea { get; set; }
+        public string NombreStatus { get; set; }
+    }
+}
---
+++ wwwroot/js/FlSolicitudes.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+        $("#tabla-solicitudes").DataTable({
+            "language": {
+                "url": "//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json"
+            }
+        });
+    });
