1ef26e160d0e0cfd1b99e1f2e6f5cb87ab5f2dd9
juarezG2025 <gerardo.juarez@contecsalatam.com>
2025-06-05 17:42:31 -0600

    Haciendo globales las funciones de JS

--- wwwroot/js/FlSolicitudes.js
+++ wwwroot/js/FlSolicitudes.js
@@ -4,38 +4,18 @@ $(document).ready(function() {
             "url": "//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json"
         }
     });
-
+    
     $(".verDetalle").on('click',async function(){
-        let id = $(this).data('id');
-        console.log(id);
-        let html;
 
-        //Deberia de exisitr una funcion para que solo llame las vistas y no repetir este codigo, implementarlo luego, pasa lo mismo para envio de datos
-        try {
-            await $.ajax({
-            url: 'Details/'+id,
-            method: 'GET'
-        })
-        .done((respuesta) => {
-            html = respuesta;
-        })
-        .fail((jqXHR, errorThrown) => {
-            console.error('Error en la petición:', {
-                status: jqXHR.status,
-                message: errorThrown,
-                response: jqXHR.responseJSON
-            });
-            html = '<div/>Estamos teniendo problemas para mostrar esta vista :c <div>';
-        })
-        .always(() => {
-            console.log('Petición completada');
-        });
-        } catch (error) {
-            
-        }
-       
+        let id = $(this).data('id');
+        mostrarSwalCarga("Buscando datos...");
+
+        let html = await obtenerVista('Details/'+id);
+        
+        swal.close();
         $("#cuerpoModal").html(html);
         $("#modalGeneral").modal('show');
         $("#modalGeneraltitulo").text('Detalle de la solicitud');
+
     });
 });
